ff7d9a07877c2da2c05de7f8aa2617b95c1ef131
Yushin Washio <dev@yuwash.eu>
2025-02-12 09:27:13 +0100

    Add github pages config

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite';
 import { svelte } from '@sveltejs/vite-plugin-svelte';
 
 export default defineConfig({
+  base: '/face-index/',
   plugins: [
     svelte({
       compilerOptions: {
